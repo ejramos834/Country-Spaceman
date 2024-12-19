423e8c19da0c3ee922e8ec8154d881d9adb4b196
edramos834@icloud.com <edramos834@icloud.com>
2024-12-18 21:00:29 -0500

    last commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,51 @@
-const countries = ["Japan"]
+const countries = ['USA', 'CANADA', 'INDIA', 'AUSTRALIA', 'GERMANY', 'FRANCE', 'SPAIN'];
+let chosenCountry = '';
+let displayedWord = [];
+let guessedLetters = [];
+let remainingGuesses = 10;
 
-let playerChoice;
-let pickedLetters;
+function startGame() {
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    chosenCountry = countries[randomIndex].toUpperCase();
+    displayedWord = Array(chosenCountry.length).fill('_');
+    guessedLetters = [];
+    remainingGuesses = 10;
+
+    updateGameDisplay();
+}
+
+function updateGameDisplay() {
+    document.getElementById('word-display').textContent = displayedWord.join(' ');
+    document.getElementById('used-letters').textContent = `Used Letters: ${guessedLetters.join(', ')}`;
+    document.getElementById('remaining-guesses').textContent = `Guesses Remaining: ${remainingGuesses}`;
+    
+    if (displayedWord.join('') === chosenCountry) {
+        document.getElementById('game-status').textContent = 'You Win!';
+    } else if (remainingGuesses === 0) {
+        document.getElementById('game-status').textContent = `Game Over! The country was ${chosenCountry}.`;
+    }
+}
+
+function handleGuess() {
+    const guess = document.getElementById('guess-input').value.toUpperCase();
+    if (guess && !guessedLetters.includes(guess) && guess.length === 1) {
+        guessedLetters.push(guess);
+
+        if (chosenCountry.includes(guess)) {
+            for (let i = 0; i < chosenCountry.length; i++) {
+                if (chosenCountry[i] === guess) {
+                    displayedWord[i] = guess;
+                }
+            }
+        } else {
+            remainingGuesses--;
+        }
+
+        document.getElementById('guess-input').value = '';
+        updateGameDisplay();
+    }
+}
+
+document.getElementById('guess-button').addEventListener('click', handleGuess);
+
+startGame();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Screenshot
+
+# About my game!
+My games name is Country Spaceman. The objective is to guess the correct country before you run out of guesses. Each time you play, you will recieve 10 guesses! It's simple but fun!
+
+
+# Deployed game limk:
+
+# Technologies used: 
+    * CSS 
+    * HTML 
+    * Javascript
+
+# 
+
+# Next Steps: 
+In the future I want to make a little more interactive. I want to add an actual spaceman that pops up with the flag of the guessed country. I really want to figure out how to use images for styling. I will work on including some hints. I want to add a very start here page.
